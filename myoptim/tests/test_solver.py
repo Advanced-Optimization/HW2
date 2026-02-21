6443df6fddb07e06af9eb1dce3a904339e529a4b
zhongqi <zhongqi2@andrew.cmu.edu>
2026-02-20 21:44:58 -0500

    HW2-2 release

--- myoptim/tests/test_solver.py
+++ myoptim/tests/test_solver.py
@@ -2,19 +2,34 @@ import numpy as np
 
 from myoptim.solver import (
     damped_newton_eq_qp,
-    augmented_lagrangian_eq_qp
+    augmented_lagrangian_eq_qp,
+    sqp_eq_newton,
+    alm_solve,
 )
 
 # ============================================================
 # Run all tests
 # ============================================================
 
+
 def test_damped_newton_eq_qp():
     NotImplementedError
+
 
 def test_augmented_lagrangian_eq_qp():
     NotImplementedError
 
+
+def test_sqp_eq_newton():
+    NotImplementedError
+
+
+def test_alm_solve():
+    NotImplementedError
+
+
 if __name__ == "__main__":
     test_damped_newton_eq_qp()
     test_augmented_lagrangian_eq_qp()
+    test_sqp_eq_newton()
+    test_alm_solve()
